84fc73f3fd845d8fa7265f4eb42ea38b001ed057
Joel MITONDO <mitondomohindo@gmail.com>
2026-08-05 23:03:33 +0100

    fix(index): correction section vision et engagement

--- SRC/JS/app.js
+++ SRC/JS/app.js
@@ -1,6 +1,7 @@
 import {afficherEtCacher, modeClairEtSombre, animationCompteurIndex} from './scripts.js';
+import {miseEnPage} from './miseEnPage.js';
 
 afficherEtCacher()
 modeClairEtSombre()
 animationCompteurIndex()
-
+miseEnPage()
---
+++ SRC/JS/donnees.js
@@ -0,0 +1,52 @@
+const informationCommission = {
+  "nom_commission": "Commission des Jeunes Notre-Dame de la Sagesse",
+  "sigle": "CDJ NODASA",
+  "slogans": [
+    "Une jeunesse unie dans la foi et l'action",
+    "Savoir, Foi et Service au cœur du Campus"
+  ],
+  "verset_accroche": "Que personne ne méprise ta jeunesse ; mais sois un modèle pour les fidèles, en parole, en conduite, en charité, en foi, en pureté.",
+  "reference_biblique": "1 Timothée 4:12",
+  "adresse_gmail": "cdj.nodasa.unikin@gmail.com",
+  "lien_youtube": "https://www.youtube.com/@cdjnodasa",
+  "membres_bureau": [
+    {
+      "id": "president",
+      "poste": "Président",
+      "nom": "Gratien MBEY",
+      "telephone": "+243 825 042 011"
+    },
+    {
+      "id": "vice-president",
+      "poste": "Vice-Président",
+      "nom": "Amos",
+      "telephone": "+243 813 220 969"
+    },
+    {
+      "id": "secretaire",
+      "poste": "Secrétaire Général",
+      "nom": "Rosario KASSONGO",
+      "telephone": "+243 816 678 703"
+    },
+    {
+      "id": "vice-secretaire",
+      "poste": "Vice-Secrétaire",
+      "nom": "Amerack MAVINGA",
+      "telephone": "+243 823 261 477"
+    },
+    {
+      "id": "tresorier",
+      "poste": "Trésorier",
+      "nom": "Christian Lukusa",
+      "telephone": "+243 900 000 555"
+    },
+    {
+      "id": "tresorier-adjoint",
+      "poste": "Trésorier Adjoint",
+      "nom": "Sarah Kanyinda",
+      "telephone": "+243 840 000 666"
+    }
+  ]
+}
+
+   
